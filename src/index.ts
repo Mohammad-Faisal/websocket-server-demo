e7b57f13f74fb160171592a9082feffab98ffd78
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2023-03-09 01:12:33 +0600

    websocket working

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,41 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import http from 'http';
+import { Server } from 'socket.io';
 const app: Application = express();
-const PORT = 3000;
+const PORT = 4000;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async (req: Request, res: Response): Promise<Response> => {
-  return res.status(200).send({
-    message: 'Hello World!',
+io.on('connection', (socket) => {
+  console.log('New client is connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+
+  socket.on('client-message', (msg) => {
+    console.log('message: ' + msg);
   });
 });
 
-app.post('/post', async (req: Request, res: Response): Promise<Response> => {
-  console.log(req.body);
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  io.emit('message', 'Hello World!');
   return res.status(200).send({
-    message: 'Hello World from post!',
+    message: 'Hello World! from server',
   });
 });
 
 try {
-  app.listen(PORT, (): void => {
+  server.listen(PORT, (): void => {
     console.log(`Connected successfully on port ${PORT}`);
   });
 } catch (error: any) {
